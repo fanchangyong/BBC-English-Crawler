4674c4678e5162f4dc5cabfc1067a39f4c0b16f5
fanchangyong <fanchangyong@gmail.com>
2021-12-24 12:02:12 +0800

    Add crawler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CrawlTask } from './crawlTask';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CrawlTask],
 })
 export class AppModule {}
---
+++ src/crawlTask.ts
@@ -0,0 +1,101 @@
+import * as fs from 'fs';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const BASE_URL = 'https://www.bbc.co.uk';
+const FILE_NAME = 'phrases.json';
+
+type Phrase = {
+  id: string;
+  title: string;
+  url: string;
+  imageURL: string;
+  desc?: string;
+  sentences?: string[];
+};
+
+@Injectable()
+export class CrawlTask {
+  private readonly logger = new Logger(CrawlTask.name);
+
+  getIdFromLink(link: string): string {
+    const lastIndex = link.lastIndexOf('/ep-');
+    const id = link.substring(lastIndex + 4);
+    return id;
+  }
+
+  getExistingPhrasesMap() {
+    let phrases: Phrase[] = [];
+    try {
+      const content = fs.readFileSync(FILE_NAME, 'utf8');
+      phrases = JSON.parse(content) as Phrase[];
+    } catch {}
+
+    const phrasesMap = phrases.reduce((acc, cur) => {
+      acc[cur.id] = cur;
+      return acc;
+    }, {});
+    return phrasesMap;
+  }
+
+  async getPhrases(): Promise<Phrase[]> {
+    const res = await axios.get(
+      `${BASE_URL}/learningenglish/chinese/features/todays-phrase`,
+    );
+
+    const phrases: Phrase[] = [];
+    const $ = cheerio.load(res.data);
+    $('.course-content-item').each((i, el) => {
+      const link = $(el).find('.text h2 a').first().attr('href');
+      const id = this.getIdFromLink(link);
+      const url = `${BASE_URL}${link}`;
+      const imageURL = $(el).find('.img a img').first().attr('src');
+      const title = $(el).find('.text h2 a').first().text();
+      console.log('title: ', title, ', link: ', link);
+      const phrase = {
+        id,
+        title,
+        url,
+        imageURL,
+      };
+      phrases.push(phrase);
+    });
+    return phrases;
+  }
+
+  async getPhraseDetail(url: string, phrase: Phrase) {
+    console.log('## getting detail from: ', url);
+    const res = await axios.get(url);
+    const $ = cheerio.load(res.data);
+    const desc = $('.widget-richtext .text p').first().text();
+    const sentences = [];
+    $('h3:contains(例句) ~ p').each((i, el) => {
+      const html = $(el).html();
+      const sentence = html.replace('<br>', '\n');
+      sentences.push(sentence);
+    });
+    phrase.desc = desc;
+    phrase.sentences = sentences;
+  }
+
+  @Timeout(3)
+  async handleCron() {
+    this.logger.debug('Crawl task called');
+
+    const existingPhrasesMap = this.getExistingPhrasesMap();
+
+    const phrases = await this.getPhrases();
+    console.log('got phrases: ', phrases)
+    for (const phrase of phrases) {
+      const { id, url } = phrase;
+      const existingPhrase = existingPhrasesMap[id];
+      if (!existingPhrase || !existingPhrase.desc) {
+        await this.getPhraseDetail(url, phrase);
+        console.log('phrase detail: ', phrase)
+      }
+      fs.writeFileSync('phrases.json', JSON.stringify(phrases, null, 4));
+    }
+  }
+}
